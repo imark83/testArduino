09a1a98076f2eb51effeaa32629b9737d78ded77
Marcos Rodríguez <marcos@unizar.es>
2014-12-18 13:22:19 +0100

    termios control working

--- testTermios.c
+++ testTermios.c
@@ -1,22 +1,58 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <termios.h>
+ 
+/* My Arduino is on /dev/ttyACM0 */
+char *portname = "/dev/ttyACM0";
+char data[3];
+ 
+int main(int argc, char **argv) {
+	int arduino;
+	int k=0;
+ 
+	/* Open the file descriptor in 
+	   READ ONLY
+	   non-blocking mode */
+	if (argc > 1) 
+		arduino = open(argv[1], O_RDONLY | O_NOCTTY);
+	else
+ 		arduino = open("/dev/ttyACM0", O_RDONLY | O_NOCTTY);
+	/* Set up the control structure */
+	struct termios toptions;
+ 
+	/* Get currently set options for the tty */
+	tcgetattr(arduino, &toptions);
 
-int main () {
-
-	FILE *arduino = fopen ("/dev/ttyACM0", "r");
-	void *data = (void*) malloc (3);
-	fflush (arduino);
-	unsigned k=0;
+	/* Set custom options */
+	/* 9600 baud */
+	cfsetispeed(&toptions, B9600);	
+ 
+	/* wait for 3 bytes to come in before read returns */
+	toptions.c_cc[VMIN] = 3;
+	/* no minimum time to wait before read returns */
+	toptions.c_cc[VTIME] = 0;
+ 
+	/* commit the options */
+	tcsetattr(arduino, TCSANOW, &toptions);
+ 
+	/* Wait for the Arduino to reset */
+	/* Flush anything already in the serial datafer */
+	tcflush(arduino, TCIFLUSH);
+	
 	while (1) {
-		if (fread(data, 1, 3, arduino) > 0) {
-			usleep (10000);
-			if (*((char*) data) != 'c') exit (0);
-			printf ("%10u: input -> ", k++);
+		printf ("%10u: input -> ", k++);
+		int serialAvailable;
+		ioctl (arduino, FIONREAD, &serialAvailable);
+		if (serialAvailable) {
+			read(arduino, data, 3);
+ 			if (data[0] != 'c') exit (0);
 			//printf ("'%c', '%hi'\n", *((char*) data), *((short int*) (data+1)));
 			printf ("'%c', '%+3hhi', '%hhu'\n", *((char*) data), *((unsigned char*) (data+1))-128, *((char*) (data+2)));
-		} 
-		usleep(40000);
-		fflush (arduino);
+		} else printf ("\n");
+		usleep (50*1000);
 	}
+	return 0;
 }
